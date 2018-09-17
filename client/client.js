7789e22e498b1a16cd43228c95b6204a2a3990c6
Nick Rook <rooknj@outlook.com>
2018-09-17 18:49:39 -0400

    Added correct actions to the response

--- client/client.js
+++ client/client.js
@@ -1,16 +1,32 @@
-const net = require('net');
+const net = require("net");
 
 const client = new net.Socket();
-client.connect(8080, '10.0.2.106', () => {
-	console.log('Connected');
-	client.write('Hello, server! Love, Client.');
+client.connect(
+  8080,
+  "10.0.2.106",
+  () => {
+		console.log("Connected");
+		let request = {
+			type: "emoji",
+			name: "cute bear"
+		}
+    client.write(JSON.stringify(request));
+  }
+);
+
+client.on("data", data => {
+	console.log("Received: " + data);
+	const response = JSON.parse(data);
+	if(response["code"] === "ヽ(´▽`)/") {
+		// Good request
+		console.log("Response body:", response["body"])
+	} else {
+		// Bad request
+		console.log("Bad request");
+	}
+  client.destroy(); // kill client after server's response
 });
 
-client.on('data', data => {
-	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
-});
-
-client.on('close', () => {
-	console.log('Connection closed');
+client.on("close", () => {
+  console.log("Connection closed");
 });
