52e677e9954da2dbffd08e91f4d44b40dd755d4b
Nick Rook <rooknj@outlook.com>
2018-09-17 18:14:08 -0400

    changed everything to arrow functions

--- client/client.js
+++ client/client.js
@@ -1,16 +1,16 @@
 const net = require('net');
 
 const client = new net.Socket();
-client.connect(1337, '127.0.0.1', function() {
+client.connect(8080, '10.0.2.106', () => {
 	console.log('Connected');
 	client.write('Hello, server! Love, Client.');
 });
 
-client.on('data', function(data) {
+client.on('data', data => {
 	console.log('Received: ' + data);
 	client.destroy(); // kill client after server's response
 });
 
-client.on('close', function() {
+client.on('close', () => {
 	console.log('Connection closed');
 });
