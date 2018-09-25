5dbf1f5c52d86b70bdb59186d5a2a62b75d0d827
Nick Rook <rooknj@outlook.com>
2018-09-25 16:14:45 -0400

    added client support for long messages

--- client/client.js
+++ client/client.js
@@ -1,30 +1,49 @@
 const net = require("net");
 
+const message = "";
+
 const client = new net.Socket();
 client.connect(
   8080,
-  "10.0.2.106",
+  "127.0.0.1",
   () => {
-		console.log("Connected");
-		let request = {
-			type: "emoji",
-			name: "cute bear"
-		}
-    client.write(JSON.stringify(request));
+    console.log("Connected");
+    // Request Emoji
+    const emojiRequest = {
+      type: "emoji",
+      name: "cute bear"
+    };
+    client.write("::" + JSON.stringify(emojiRequest) + "::");
+
+    // Request Meme
+    const memeRequest = {
+      type: "meme"
+    };
+    client.write("::" + JSON.stringify(memeRequest) + "::");
   }
 );
 
 client.on("data", data => {
-	console.log("Received: " + data);
-	const response = JSON.parse(data);
-	if(response["code"] === "ヽ(´▽`)/") {
-		// Good request
-		console.log("Response body:", response["body"])
-	} else {
-		// Bad request
-		console.log("Bad request");
+	console.log("DATA")
+	if(data.toString().startsWith("::")) {
+		console.log("Starts");
+		message="";
 	}
-  client.destroy(); // kill client after server's response
+	message += data.toString();
+	if(data.toString.endsWith("::")) {
+		console.log("ends");
+		console.log(message.substring(2, message.length-2));
+	}
+  //console.log("Received: " + data);
+  //const response = JSON.parse(data);
+  // if(response["code"] === "ヽ(´▽`)/") {
+  // 	// Good request
+  // 	console.log("Response body:", response["body"])
+  // } else {
+  // 	// Bad request
+  // 	console.log("Bad request");
+  // }
+  //client.destroy(); // kill client after server's response
 });
 
 client.on("close", () => {
