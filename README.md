def513c3649d37ed2fa74dd6f3b7f1ff49dbe6cc
Noah Bass <bassnh@mail.uc.edu>
2018-09-25 12:25:23 -0400

    Update server.js

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Emoji-Protocol
-Making a communication protocol with ascii emojis
+# Emoji Protocol
+
+> Making a communication protocol with ascii emojis and memes.
+
+Wikipedia Meme By That's Pretty Good [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0), from Wikimedia Commons
+
+__Example Protocol Implementation__
+
+Requirements: Node.js 8.0.0 or later
--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
-const net = require('net');
+const net = require('net')
+const fs = require('fs')
+
+const readFile = (filename) => {
+    return fs.readFileSync(`./${filename}`)
+}
 
 // ASCII emoji data from https://github.com/dysfunc/ascii-emoji
 const emojiTable = {
@@ -20,34 +25,60 @@ const emojiTable = {
     "happy face": "ヽ(´▽`)/"
 }
 
+// Helper to send a bad request response 
+const badRequest = (socket) => {
+    const response = { code: emojiTable['reddit disapproval face'] }
+    socket.write(JSON.stringify(response))
+}
+
 const server = net.createServer(socket => {
     socket.on('data', data => {
-        const dataText = data.toString()
-        // parse the incoming text as json
-        const dataJson = JSON.parse(dataText)
-        console.log(dataJson)
+        // parse the incoming data as json
+        const dataJson = JSON.parse(data)
+        console.log('Incoming data:', dataJson)
 
         const requestType = dataJson['type'] || undefined
         const requestName = dataJson['name'] || undefined
 
         if (!requestType) {
             // bad request
-            const response = { code: emojiTable['reddit disapproval face'] }
-            socket.write(JSON.stringify(response))
+            badRequest(socket)
         }
 
-        const emoji = emojiTable[requestName]
-        if (requestName && emoji) {
+        if (requestType === 'emoji') {
+            const emoji = emojiTable[requestName]
+            if (requestName && emoji) {
+                const response = {
+                    code: emojiTable['happy face'],
+                    body: emoji
+                }
+                socket.write(JSON.stringify(response))
+            } else {
+                // bad request
+                badRequest(socket)
+            }
+        } else if (requestType === 'meme') {
+            // read the meme data from file system
+            const fileData = readFile('wikipedia-meme.jpg')
+            // respond with image data
             const response = {
                 code: emojiTable['happy face'],
-                body: emoji
+                body: fileData,
+                type: 'image/jpg'
             }
             socket.write(JSON.stringify(response))
         } else {
             // bad request
-            const response = { code: emojiTable['reddit disapproval face'] }
-            socket.write(JSON.stringify(response))
+            badRequest(socket)
         }
+    })
+
+    socket.on('error', () => {
+        console.log('Error!')
+    })
+
+    socket.on('end', () => {
+        console.log('done!')
     })
 
 	socket.pipe(socket)
@@ -58,6 +89,4 @@ server.on('error', error => {
     server.end()
 })
 
-server.listen(8080, '10.0.2.106')
-
-
+server.listen(8080, '127.0.0.1')
